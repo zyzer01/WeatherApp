7e538fccd3e4356899a1314708b749a2c1692bd6
zyzer01 <davidicfola@gmail.com>
2022-08-18 21:56:22 +0100

    Fix: HTML rendering for safari

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.post("/", function(req, res){
             const icon = weatherData.weather[0].icon
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write("<h1>The weather in " + query + " today is " + temp + " degree celsius</h1>");
             res.write("<h2>It is " + weatherDescription + "</h2>");
             res.write("<img src=" + imageURL + ">");
